328448d8c10c089c79611184190f5dce48919782
mcolon45 <mcolon45@users.noreply.github.com>
2025-03-16 21:50:11 -0500

    Update index.js
    adding a border to the div for the hex stage

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import HexagonalScene from '../components/HexagonalGrid';
 
 export default function Home() {
   return (
-    <div style={{ width: '100vw', height: '100vh', background: '#111' }}>
+    <div style={{ width: '100vw', height: '100vh', background: '#111' border:'1px solid #000000' }}>
       <HexagonalScene />
     </div>
   );
