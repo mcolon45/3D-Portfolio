ed334f6e6fcd8d26ed5b426b554e44fa8d5b6251
mcolon45 <mcolon45@users.noreply.github.com>
2025-03-17 00:12:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import HexagonalScene from '../components/HexagonalGrid';
 
 export default function Home() {
   return (
-    <div style={{ width: '100vw', height: '100vh', background: '#111' border:'1px solid #FFFFFF' }}>
+    <div style={{ width: '100vw', height: '100vh', background: '#111', border:'1px solid #FFFFFF' }}>
       <HexagonalScene />
     </div>
   );
